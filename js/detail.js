3882c6319ff5c266f5f99655347751dd2ea0f19e
ThoiThanhVuong <136805692+ThoiThanhVuong@users.noreply.github.com>
2023-11-24 16:35:39 +0700

    Add files via upload

--- js/detail.js
+++ js/detail.js
@@ -11,15 +11,17 @@ const btnDetail = key => {
       `
                 <div class="container">
                     <div class="row">
-                        <div class="detail-product-left">
+                      <div class="detail-innerwrap">
+                          <div class="detail-product-left">
                             <img src="${detail[i].thumbnail}" alt="">
-                        </div>
-                        <div class="detail-product-right">
-                            <p><strong>Product Name: ${detail[i].title} </strong></p>
-                            <p><strong>Description: </strong></p>
-                            <p><strong>Price: ${detail[i].price}</strong></p>
-                            <p><strong>saleprice: ${detail[i].saleprice} </strong></p>
-                            <button onclick = "addToCard(${detail[i].id})">Add To Card</button>
+                          </div>
+                          <div class="detail-product-right">
+                              <p><strong>Product Name: ${detail[i].title} </strong></p>
+                              <div class="price">Price: ${detail[i].price}</div>
+                              <div class="saleprice">Sale Price: ${detail[i].saleprice}</div>
+                              <div class="desc">${detail[i].description}</div>
+                              <button onclick = "addToCard(${detail[i].id})">Add To Card</button>
+                          </div>
                         </div>
                     </div>
                 </div>
