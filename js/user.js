acffd851ade8aada98991a1895cf410c603f7869
Vhuu08092004 <133578866+Vhuu08092004@users.noreply.github.com>
2023-12-06 20:19:16 +0700

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,55 +1,56 @@
 function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) === " ") {
-      c = c.substring(1);
+    var name = cname + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
     }
-    if (c.indexOf(name) === 0) {
-      return c.substring(name.length, c.length);
-    }
+    return "";
   }
-  return "";
-}
-function deleteCookie(cname) {
-  document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
-}
-const user = document.querySelector(".user");
-let newHTML;
-
-const conditonUser = getCookie("token");
-
-const Login = () => {
-  window.location.href = "login.html";
-};
-
-const Register = () => {
-  window.location.href = "register.html";
-};
-
-const Logout = () => {
-  deleteCookie("token");
-  deleteCookie("user");
-  window.location.reload();
-};
-// let data = JSON.parse(localStorage.getItem())
-if (conditonUser) {
-  newHTML = `
-        <i class="fa-regular fa-user"></i>
-        <span>Chào khách hàng</span>
-
-        <ul user-menu>
-            <li class="userProfile">Thông tin khách hàng</li>
-            <li onclick="Logout()">Đăng Xuất</li>        
-        </ul>
-        `;
-  user.innerHTML = newHTML;
-} else {
-  newHTML = `
-            <div onclick = "Login()">Đăng nhập</div>
-            <div onclick="Register()"> Đăng kí </div>
-        `;
-
-  user.innerHTML = newHTML;
-}
+  function deleteCookie(cname) {
+    document.cookie = `${cname}=; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
+  }
+  const user = document.querySelector(".user");
+  let newHTML;
+  
+  const conditonUser = getCookie("token");
+  
+  const Login = () => {
+    window.location.href = "login.html";
+  };
+  
+  const Register = () => {
+    window.location.href = "register.html";
+  };
+  
+  const Logout = () => {
+    // deleteCookie("token");
+    deleteCookie("user");
+    window.location.reload();
+  };
+  // let data = JSON.parse(localStorage.getItem())
+  if (conditonUser) {
+    newHTML = `
+          <i class="fa-regular fa-user"></i>
+          <span>Chào khách hàng</span>
+  
+          <ul user-menu>
+              <li class="userProfile">Thông tin khách hàng</li>
+              <li onclick="Logout()">Đăng Xuất</li>        
+          </ul>
+          `;
+    user.innerHTML = newHTML;
+  } else {
+    newHTML = `
+              <div onclick = "Login()">Đăng nhập</div>
+              <div onclick="Register()"> Đăng kí </div>
+          `;
+  
+    user.innerHTML = newHTML;
+  }
+  
